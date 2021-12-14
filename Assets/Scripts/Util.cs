30606c15b7b370b7add2887e3c7cf49e3cc25625
KevinSaey <36826320+KevinSaey@users.noreply.github.com>
2021-12-14 22:01:35 +0000

    Unity update
    Added Util funciton
    Added scene for brute force filler

--- Assets/Scripts/Util.cs
+++ Assets/Scripts/Util.cs
@@ -5,6 +5,19 @@ using UnityEngine;
 //static classes can be accessed in the entire solution without creating an object of the class
 public static class Util
 {
+    /// <summary>
+    /// Extension method to Unities Vector3Int class. Now you can use a Vector3 variable and use the .ToVector3InRound to get the vector rounded to its integer values
+    /// </summary>
+    /// <param name="v">the Vector3 variable this method is applied to</param>
+    /// <returns>the rounded Vector3Int value of the given Vector3</returns>
+    public static Vector3Int ToVector3IntRound(this Vector3 v) => new Vector3Int(Mathf.RoundToInt(v.x), Mathf.RoundToInt(v.y), Mathf.RoundToInt(v.z));
+    public static bool TryOrientIndex(Vector3Int localIndex, Vector3Int anchor, Quaternion rotation, Vector3Int gridDimensions, out Vector3Int worldIndex)
+    {
+        var rotated = rotation * localIndex;
+        worldIndex = anchor + rotated.ToVector3IntRound();
+        return CheckInBounds(gridDimensions, worldIndex);
+    }
+
     public static List<Vector3Int> Directions = new List<Vector3Int>
     {
         new Vector3Int(-1,0,0),// min x
